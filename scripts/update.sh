947a6d9439932dbf3b1ba449a2040fb97a048090
Dev Patel <dvptl68@gmail.com>
2020-07-25 09:58:24 -0400

    Import scripts

---
+++ scripts/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d .git ]; then
+  read -p "Enter the commit message: "  message
+  read -p "Would you like to commit and push all changes with the following message: $(tput bold)${message}$(tput sgr0) (y/n)? "  confirm
+  if [ "$confirm" = "y" ]; then
+    git add .
+    git commit -m "$message"
+    git push
+    echo
+    echo "$(tput bold)Staged, committed, and pushed changes!$(tput sgr0)"
+  else
+    echo
+    echo "$(tput bold)Process terminated.$(tput sgr0)"
+  fi
+else
+  echo
+  echo "$(tput bold)You must run this script from a Git repository directory!$(tput sgr0)"
+fi
