9209022625646adaca6cc20c01adf0d03074fc3a
Dev Patel <dvptl68@gmail.com>
2020-07-27 19:07:39 -0400

    Improve code structure

--- src/bot.py
+++ src/bot.py
@@ -20,32 +20,25 @@ class MyBot(BaseAgent):
   # Track boost pad information
   def initialize_agent(self): self.boost_pad_tracker.initialize_boosts(self.get_field_info())
 
-  # Main controller function
+  # Main controller function - called many times per second
   def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
 
-    """
-    This function will be called by the framework many times per second. This is where you can
-    see the motion of the ball, etc. and return controls to drive your car.
-    """
-
-    # Keep our boost pad info updated with which pads are currently active
+    # Get currently active boost pad info
     self.boost_pad_tracker.update_boost_status(packet)
 
-    # This is good to keep at the beginning of get_output. It will allow you to continue
-    # any sequences that you may have started during a previous call to get_output.
+    # Continue sequences from previous call
     if self.active_sequence and not self.active_sequence.done:
       controls = self.active_sequence.tick(packet)
-      if controls is not None:
-        return controls
+      if controls is not None: return controls
 
-    # Gather some information about our car and the ball
+    # Gather information about car and ball
     my_car = packet.game_cars[self.index]
     car_location = Vec3(my_car.physics.location)
     car_velocity = Vec3(my_car.physics.velocity)
     ball_location = Vec3(packet.game_ball.physics.location)
 
     if car_location.dist(ball_location) > 1500:
-      # We're far away from the ball, let's try to lead it a little bit
+      # Set path of car to future ball location
       ball_prediction = self.get_ball_prediction_struct()  # This can predict bounces, etc
       ball_in_future = find_slice_at_time(ball_prediction, packet.game_info.seconds_elapsed + 2)
       target_location = Vec3(ball_in_future.physics.location)
@@ -53,30 +46,24 @@ class MyBot(BaseAgent):
     else:
       target_location = ball_location
 
-    # Draw some things to help understand what the bot is thinking
+    # Draw rendering lines
     self.renderer.draw_line_3d(car_location, target_location, self.renderer.white())
     self.renderer.draw_string_3d(car_location, 1, 1, f'Speed: {car_velocity.length():.1f}', self.renderer.white())
     self.renderer.draw_rect_3d(target_location, 8, 8, True, self.renderer.cyan(), centered=True)
 
-    if 750 < car_velocity.length() < 800:
-      # We'll do a front flip if the car is moving at a certain speed.
-      return self.begin_front_flip(packet)
+    # Front flip if car is moving at a certain speed
+    if 750 < car_velocity.length() < 800: return self.begin_front_flip(packet)
 
     controls = SimpleControllerState()
     controls.steer = steer_toward_target(my_car, target_location)
     controls.throttle = 1.0
-    # You can set more controls if you want, like controls.boost.
 
     return controls
 
   # Car flip function
   def begin_front_flip(self, packet):
-    
-    # Send some quickchat just for fun
-    self.send_quick_chat(team_only=False, quick_chat=QuickChatSelection.Information_IGotIt)
 
-    # Do a front flip. We will be committed to this for a few seconds and the bot will ignore other
-    # logic during that time because we are setting the active_sequence.
+    # Do a front flip
     self.active_sequence = Sequence([
       ControlStep(duration=0.05, controls=SimpleControllerState(jump=True)),
       ControlStep(duration=0.05, controls=SimpleControllerState(jump=False)),
@@ -84,5 +71,5 @@ class MyBot(BaseAgent):
       ControlStep(duration=0.8, controls=SimpleControllerState()),
     ])
 
-    # Return the controls associated with the beginning of the sequence so we can start right away.
+    # Return the controls associated with the beginning of the sequence
     return self.active_sequence.tick(packet)
